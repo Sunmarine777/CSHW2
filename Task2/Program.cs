5c9525550b39bd81a4be315ca6c380ad3e620320
Sunmarine <Sunmarine777@yandex.ru>
2023-11-22 23:50:11 +0300

    Task2 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter number <> 0 for X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number <> 0 for Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+      {Console.WriteLine("Quoter 1");
+return;}
+
+if (X < 0 && Y > 0)
+      {Console.WriteLine("Quater 2");
+return;}
+
+if (X < 0 && Y < 0)
+      {Console.WriteLine("Quater 3");
+return;}
+
+else
+      Console.WriteLine("Quater 4");
