f3846acc8c8663519eb4367eca6dac87c220ec88
Sunmarine <Sunmarine777@yandex.ru>
2023-11-22 23:16:31 +0300

    Task done

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter any number ");
+int dividable = Convert.ToInt32(Console.ReadLine());
+
+int firstDivisor = 7;
+int secondDivisor = 23;
+
+if (dividable % firstDivisor == 0 && dividable % secondDivisor == 0)
+
+      Console.WriteLine("Multiple 7 and 23");
+
+else
+      Console.WriteLine("Not multiple 7 and 23");
