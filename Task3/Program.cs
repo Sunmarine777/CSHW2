2b08d7f7f468db9b00eb8c4bd2dc67746f45fe72
Sunmarine <Sunmarine777@yandex.ru>
2023-11-25 23:06:30 +0300

    Task3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter any integer number from 10 to 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 10;
+int b = number % 10;
+
+if (number >= 10 && number <= 99 && a > b)
+    { 
+      Console.WriteLine(a);
+    }
+ 
+if (number >= 10 && number <= 99 && a < b)
+    { 
+      Console.WriteLine(b);
+    }
+    
+if (number >= 10 && number <= 99 && a == b)
+    { 
+      Console.WriteLine(a);
+    }
+else
+Console.WriteLine("Not match conditions");
